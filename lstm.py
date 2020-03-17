70a5b94b91b089f973ebfcb55d663be2c4bc7a41
void-zero <wallace.lucena@hotmail.com>
2020-03-17 01:41:04 -0300

    Added a new converter

--- lstm.py
+++ lstm.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Feb 22 15:53:57 2020
+Created on Sat Mar 14 20:24:10 2020
 
-@author: j-wallace
+@author: itamar
 """
+
+
 from __future__ import print_function
-from keras.callbacks import LambdaCallback
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import CuDNNLSTM, Dropout
-#from keras.optimizers import RMSprop
+from tensorflow.keras.callbacks import LambdaCallback
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM, Dropout
 import numpy as np
 import random
 import sys
 import io
 import tensorflow as tf
 
-
-gpus = tf.config.experimental.list_physical_devices('GPU')
+gpus = tf.config.list_physical_devices('GPU')
 if gpus:
     print('Using GPU')
     try:
         # Currently, memory growth needs to be the same across GPUs
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
-            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            logical_gpus = tf.config.list_logical_devices('GPU')
             print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
     except RuntimeError as e:
         # Memory growth must be set before GPUs have been initialized
@@ -66,21 +66,20 @@ for i, sentence in enumerate(sentences):
 print('Build model...')
 model = Sequential()
 
-model.add(CuDNNLSTM(256, input_shape=(maxlen, len(chars)), return_sequences=True))
+model.add(LSTM(128, input_shape=(maxlen, len(chars)), return_sequences=True))
+model.add(Dropout(0.3))
+
+model.add(LSTM(64))
 model.add(Dropout(0.2))
 
-model.add(CuDNNLSTM(256, return_sequences=True))
-model.add(Dropout(0.1))
-
-model.add(CuDNNLSTM(256))
-model.add(Dropout(0.2))
-
-model.add(Dense(256))
+model.add(Dense(128))
 model.add(Dropout(0.3))
 
 model.add(Dense(len(chars), activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+
 def sample(preds, temperature=1.0):
     # helper function to sample an index from a probability array
     preds = np.asarray(preds).astype('float64')
@@ -97,7 +96,7 @@ def on_epoch_end(epoch, _):
     print('----- Generating text after Epoch: %d' % epoch)
 
     start_index = random.randint(0, len(text) - maxlen - 1)
-    for diversity in [0.2, 0.5, 1.0, 1.2]:
+    for diversity in [0.5, 1.0, 1.2, 1.5, 2]:
         print('----- diversity:', diversity)
 
         generated = ''
@@ -106,7 +105,7 @@ def on_epoch_end(epoch, _):
         print('----- Generating with seed: ' + sentence)
         sys.stdout.write(generated)
 
-        for i in range(400):
+        for i in range(1600):
             x_pred = np.zeros((1, maxlen, len(chars)))
             for t, char in enumerate(sentence):
                 x_pred[0, t, char_indices[char]] = 1.
@@ -124,6 +123,5 @@ def on_epoch_end(epoch, _):
 print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
 
 model.fit(x, y,
-          batch_size=128,
-          epochs=60,
-          callbacks=[print_callback])
+          batch_size=1024,
+          epochs=60)
